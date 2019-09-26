f31896a3d52d89b40417e5362a9d4a7486d150c2
Paulina Opacka <paulina.opacka@gmail.com>
2019-09-26 18:09:37 +0200

    Updates

---
+++ precache-manifest.4792c326abd24425b9c5d549061e6493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1f65accc5d4f3c071caa48b4c0cdb2",
+    "url": "/cruddapp/index.html"
+  },
+  {
+    "revision": "fd299d3ad982cae239c9",
+    "url": "/cruddapp/static/css/main.53758a49.chunk.css"
+  },
+  {
+    "revision": "64f4ef062d9fdd789be5",
+    "url": "/cruddapp/static/js/2.b87c90db.chunk.js"
+  },
+  {
+    "revision": "fd299d3ad982cae239c9",
+    "url": "/cruddapp/static/js/main.5b3c25cd.chunk.js"
+  },
+  {
+    "revision": "1e933026ae0bbe9c6636",
+    "url": "/cruddapp/static/js/runtime-main.44c20c47.js"
+  },
+  {
+    "revision": "9d4d0b0e80420b98ef54432dd0fad135",
+    "url": "/cruddapp/static/media/background1.9d4d0b0e.jpg"
+  }
+]);
